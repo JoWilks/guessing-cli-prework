700b73f8cafdf2ac01c105271698875134d0c207
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-09 19:24:34 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,4 +3,10 @@ def run_guessing_game
   user_input= gets.chomp
 
   rand_number=rand(1..10)
+
+  if user_input.to_i == rand_number
+    puts "You guessed the correct number!"
+  else
+    puts "The computer guessed #{rand_number}"
+  end
 end
